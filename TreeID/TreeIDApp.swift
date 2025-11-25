8389c277695b9fe243aa8a40c0a66562b8f4eaa7
iluo <lokae0@gmail.com>
2025-11-25 14:50:17 -0800

    Initial Commit

---
+++ TreeID/TreeIDApp.swift
@@ -0,0 +1,32 @@
+//
+//  TreeIDApp.swift
+//  TreeID
+//
+//  Created by Ian Luo on 11/25/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct TreeIDApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ TreeIDTests/TreeIDTests.swift
@@ -0,0 +1,16 @@
+//
+//  TreeIDTests.swift
+//  TreeIDTests
+//
+//  Created by Ian Luo on 11/25/25.
+//
+
+import Testing
+
+struct TreeIDTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
