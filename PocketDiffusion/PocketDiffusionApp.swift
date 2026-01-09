37cd8b7b6dd526fdc8a3f8a4811fdcbe26ed18bf
iluo <lokae0@gmail.com>
2026-01-09 06:20:05 -0800

    Clear app cache on launch

--- PocketDiffusion/PocketDiffusionApp.swift
+++ PocketDiffusion/PocketDiffusionApp.swift
@@ -13,6 +13,44 @@ struct PocketDiffusionApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .onAppear() {
+                    clearAppCache()
+                }
+        }
+    }
+
+    private func clearAppCache() {
+        guard let cacheURL = FileManager.default.urls(
+            for: .cachesDirectory,
+            in: .userDomainMask
+        ).first else {
+            Log.shared.info("Could not find cache directory URL")
+            return
+        }
+
+        let fileManager = FileManager.default
+
+        do {
+            let directoryContents = try fileManager.contentsOfDirectory(
+                at: cacheURL,
+                includingPropertiesForKeys: nil,
+                options: []
+            )
+
+            for file in directoryContents {
+                do {
+                    try fileManager.removeItem(at: file)
+                } catch let error as NSError {
+                    Log.shared.info(
+                        "Error removing cache file \(file.lastPathComponent): \(error.localizedDescription)"
+                    )
+                }
+            }
+            Log.shared.info("Cache cleared successfully")
+        } catch {
+            Log.shared.info(
+                "Error accessing cache directory: \(error.localizedDescription)"
+            )
         }
     }
 }
