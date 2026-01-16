5382a7a7cc52320d1028503a63498879e30f7d9b
iluo <lokae0@gmail.com>
2026-01-15 21:21:41 -0800

    Replace ScrollView with DragGesture for more precision

--- PocketDiffusion/UI/ZoomableImageView.swift
+++ PocketDiffusion/UI/ZoomableImageView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 private extension UI {
     static let animationDuration: CGFloat = 0.25
+    static let minZoomScale: CGFloat = 1.0
     static let maxZoomScale: CGFloat = 5.0
 }
 
@@ -16,49 +17,80 @@ struct ZoomableImageView: View {
 
     var uiImage: UIImage
 
-    @State private var zoomScale: CGFloat = 1.0
-    @GestureState private var gestureScale: CGFloat = 1.0
+    @State private var currentScale: CGFloat = 1.0
+    @State private var finalScale: CGFloat = 1.0
+
+    @State private var currentOffset: CGSize = .zero
+    @State private var finalOffset: CGSize = .zero
+
+    @State private var imageSize: CGSize = .zero
 
     var body: some View {
-        GeometryReader { geometry in
-            ScrollView([.horizontal, .vertical], showsIndicators: false) {
-                Image(uiImage: uiImage)
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .scaleEffect(zoomScale * gestureScale)
-                    .frame(
-                        width: geometry.size.width * (zoomScale * gestureScale),
-                        height: geometry.size.height * (zoomScale * gestureScale)
-                    )
-                    .gesture(
-                        MagnifyGesture()
-                            .updating($gestureScale) { value, state, _ in
-                                state = value.magnification
-                            }
-                            .onEnded { value in
-                                zoomScale *= value.magnification
-
-                                if zoomScale < 1.0 {
-                                    withAnimation(.spring()) {
-                                        zoomScale = 1.0
-                                    }
-                                }
-                                if zoomScale >= UI.maxZoomScale {
-                                    withAnimation(.spring()) {
-                                        zoomScale = UI.maxZoomScale
-                                    }
-                                }
-                            }
-                    )
+        Image(uiImage: uiImage)
+            .resizable()
+            .scaledToFit()
+            .scaleEffect(currentScale)
+            .offset(currentOffset)
+            .onGeometryChange(for: CGSize.self) { proxy in
+                return proxy.size
+            } action: { newSize in
+                imageSize = newSize
             }
-            .scrollBounceBehavior(.basedOnSize)
+            .gesture(
+                MagnifyGesture()
+                    .onChanged { value in
+                        currentScale = finalScale * value.magnification
+                    }
+                    .onEnded { _ in
+                        fixBounds()
+                    }
+                    .simultaneously(
+                        with: DragGesture()
+                            .onChanged { value in
+                                currentOffset = CGSize(
+                                    width: finalOffset.width + value.translation.width,
+                                    height: finalOffset.height + value.translation.height
+                                )
+                            }
+                            .onEnded { _ in
+                                fixBounds()
+                            }
+                    )
+            )
             .onTapGesture(count: 2) {
                 // Double tap to zoom or unzoom
                 withAnimation(.easeInOut(duration: UI.animationDuration)) {
-                    let hasNotZoomed = zoomScale == 1.0
-                    zoomScale = hasNotZoomed ? 2.0 : 1.0
+                    let hasNotZoomed = currentScale == 1.0
+                    currentScale = hasNotZoomed ? 2.0 : 1.0
+                    fixBounds()
                 }
             }
+    }
+
+    private func fixBounds() {
+        withAnimation(.spring) {
+            // Snap back scale if too small or too large
+            if currentScale < UI.minZoomScale {
+                currentScale = UI.minZoomScale
+            } else if currentScale > UI.maxZoomScale {
+                currentScale = UI.maxZoomScale
+            }
+
+            // Calculate bounds based on new scale
+            // The image "overflows" the screen by this amount on each side
+            let extraWidth = (imageSize.width * currentScale - imageSize.width) / 2
+            let extraHeight = (imageSize.height * currentScale - imageSize.height) / 2
+
+            // Constrain offset so edges don't leave the screen
+            // If scale is 1.0, extraWidth is 0, so offset snaps to .zero
+            let restrictedWidth = min(max(currentOffset.width, -extraWidth), extraWidth)
+            let restrictedHeight = min(max(currentOffset.height, -extraHeight), extraHeight)
+
+            currentOffset = CGSize(width: restrictedWidth, height: restrictedHeight)
+
+            // Save state for next gesture
+            finalScale = currentScale
+            finalOffset = currentOffset
         }
     }
 }
